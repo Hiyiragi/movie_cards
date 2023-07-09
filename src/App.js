52c0d2f022963fab15647029416f634b3d4e10b4
Hiyiragi <128255052+Hiyiragi@users.noreply.github.com>
2023-07-09 19:30:11 +0200

    added basic settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+async function loadMovies() {
+  try {
+    const data = await axios(
+      "https://api.themoviedb.org/3/account/{Hiyiragi}/favorite/movies"
+    );
+    return data;
+  } catch (err) {
+    throw Error("Failed to load radio!");
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const loadData = async () => {
+      // try {
+      const data = await loadMovies();
+      setData(data);
+      // } catch (err) {
+      //   toas.error(err.message);
+      // } finally {
+      //   setIsLoading(false);
+      // }
+    };
+
+    loadData();
+  }, []);
+  console.log(data);
+  return <div>HI</div>;
 }
 
 export default App;
